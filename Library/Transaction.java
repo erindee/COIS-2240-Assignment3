800adf78df6d818cd88bc773f011f768df77e476
erindee <erindeehan@trentu.ca>
2024-11-27 14:22:38 -0500

    Completed Task 2 Question 2

--- Library/Transaction.java
+++ Library/Transaction.java
@@ -1,30 +1,32 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Transaction {
 	//Singleton
-	private static Transaction instance;
+	private static Transaction transactionInstance;
 	
-	// Constructor for preventing instantiation
+	// Constructor for preventing direct instantiation
 	private Transaction() {}
 	
 	// Get the singleton
 	public static Transaction getTransaction() {
-		if (instance == null) {
-			instance = new Transaction ();
+		if (transactionInstance == null) {
+			transactionInstance = new Transaction ();
 		}
-		return instance;
+		return transactionInstance;
     }
 	// To perform a book being borrowed
-	public boolean borrowBook(Book book, Member member) {
+	public void borrowBook(Book book, Member member) {
 		if (book.isAvailable()) {
 			book.borrowBook();
 			member.borrowBook(book);
-			System.out.println(getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed " + book.getTitle());
-            return true;
+			String transactionDetails = getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed " + book.getTitle();
+			System.out.println(transactionDetails);
+			saveTransaction(transactionDetails);
         } else {
             System.out.println("The book is not available.");
-            return false;
         }
         
 	} // To perform the return of a book
@@ -32,13 +34,23 @@ public class Transaction {
         if (member.getBorrowedBooks().contains(book)) {
             member.returnBook(book);
             book.returnBook();
-            System.out.println(getCurrentDateTime() + " - Returning: " + member.getName() + " returned " + book.getTitle());
+            String transactionDetails = getCurrentDateTime() + " - Returning: " + member.getName() + " returned " + book.getTitle();
+            System.out.println(transactionDetails);
+            saveTransaction(transactionDetails);
         } else {
             System.out.println("This book was not borrowed by the member.");
         }
-        
-    } // Get the current date and time
+    }
+    private void saveTransaction(String transactionDetails) {
+    	try (FileWriter writer = new FileWriter("transactions.txt", true)) {
+    		writer.write(transactionDetails + "\n");
+    	} catch (IOException e) {
+    		System.out.println("Error occured while saving transaction: " + e.getMessage());
+    	}
+    }
+    // Get the current date and time
     private String getCurrentDateTime() {
-        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+    	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.format(new Date());    
     }
 }
